f9353408e45389b1d173f891c5ffb002233e3893
rina <rinaticstates@gmail.com>
2022-01-08 20:13:21 -0500

    slowly more progress

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,5 +1,7 @@
-getButtons = document.querySelectorAll(".drum")
-for (let i = 0; i < 7; i++) {
+var getButtons = document.querySelectorAll(".drum")
+var audio = new Audio("sounds/tom-1.mp3");
+for (var i = 0; i < 7; i++) {
   getButtons[i].addEventListener("click", function() {
-      alert("I got clicked !");})
+    audio.play();
+});
 }

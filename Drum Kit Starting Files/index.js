313a204f3bd888281f16755bf51cffd62c849ce3
rina <rinaticstates@gmail.com>
2022-01-12 21:27:49 -0500

    let's finish this course

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -2,6 +2,7 @@ var getButtons = document.querySelectorAll(".drum")
 var audio = new Audio("sounds/tom-1.mp3");
 for (var i = 0; i < 7; i++) {
   getButtons[i].addEventListener("click", function() {
-    audio.play();
+    //audio.play();
+    this.style.color = "white";
 });
 }
